5ea826f83bcdabbfd89145d34aac007e58aecfcd
Hector0308 <a23310342@live.ceti.mx>
2024-10-26 01:58:47 +0000

    clase 5

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 int main(){
 
      TazoDorado coqueto;
-
      coqueto.Flotar();
 
      std::cout
--- src/memoria.cpp
+++ src/memoria.cpp
@@ -1,18 +1,41 @@
  #include<iostream>
+ #include<TazoDorado.hpp>
+
  using namespace std;
  
  
  int main(int argc, char const *argv[])
  {
-    int   entero = 0;
-    bool  booleano = true;
-    char  letra = 'A';
-    float decimal = 0.5;
+    // int   entero = 0;
+    // bool  booleano = true;
+    // char  letra = 'A';
+    // float decimal = 0.5;
 
-    cout<<"Tamaño entero:" << sizeof(entero)<< endl;
-    cout<<"Tamaño booleano:" << sizeof(booleano)<< endl;
-    cout<<"Tamaño letra:" << sizeof(letra)<< endl;
-    cout<<"Tamaño decimal:" << sizeof(decimal)<< endl;
+    // cout<<"Tamaño entero:" << sizeof(entero)<< endl;
+    // cout<<"Tamaño booleano:" << sizeof(booleano)<< endl;
+    // cout<<"Tamaño letra:" << sizeof(letra)<< endl;
+    // cout<<"Tamaño decimal:" << sizeof(decimal)<< endl;
+    // cout<<"Tamaño de Tazo:" << sizeof(TazoDorado)<< endl;
+    // cout<< endl;
+    
+    // TazoDorado tazos [20];
+    // for(int i = 0; i < 20; i++){
+      
+    //   cout << "Diereccion de Tazo" << i << ":" << & tazos[i] << endl;
+
+    // }
+   
+    // void* diereccion = malloc(1); 
+    TazoDorado* direccion;
+
+    direccion = (TazoDorado*)malloc(sizeof(TazoDorado));
+    direccion->Inicializar();
+     cout << "Valor en direccion :" << direccion->Flotando() << endl;
+     
+  // c++------------------------------ 
+     TazoDorado *direccion2;
+     direccion2 = new TazoDorado();
+    cout << "Valor en direccion :" << direccion2->Flotando() << endl;
 
     return 0;
  }
